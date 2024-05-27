544bb63c06b9798c3b4522db38a727f62bb8cc0a
Erwin Peterlin <erwin.peterlin@semify-eda.com>
2024-05-27 11:53:30 +0200

    update obi-wb-bridge testcase

--- core/custom/obi_wb_bridge/testbench/obi_wb_bridge_tb.sv
+++ core/custom/obi_wb_bridge/testbench/obi_wb_bridge_tb.sv
@@ -19,11 +19,69 @@ module obi_wb_bridge_tb;
         $dumpfile("wb_obi_test.vcd");
         $dumpvars();
     end
+
+
+    localparam DATAW = 32;
+    localparam ADDRW = 20;
+
+
+    wire [ADDRW-1:0]   wbm0_adr_i;
+    wire [DATAW-1:0]   wbm0_dat_i;
+    wire [DATAW-1:0]   wbm0_dat_o;
+    wire               wbm0_we_i;
+    wire               wbm0_stb_i;
+    wire               wbm0_ack_o;
+    wire               wbm0_cyc_i;
+
+    wire [ADDRW-1:0]   wbm1_adr_i;
+    wire [DATAW-1:0]   wbm1_dat_i;
+    wire [DATAW-1:0]   wbm1_dat_o;
+    wire               wbm1_we_i;
+    wire               wbm1_stb_i;
+    wire               wbm1_ack_o;
+    wire               wbm1_cyc_i;
+
+
+    wb_arbiter_2 #
+    (
+        .DATAW(DATAW),
+        .ADDRW(ADDRW)
+    ) i_wb_arbiter
+    (
+        .clk(wfg_clk),
+        .rst_n(core_rst_n),
+
+        .wbm0_adr_i(wbm0_adr_i),
+        .wbm0_dat_i(wbm0_dat_i),
+        .wbm0_dat_o(wbm0_dat_o),
+        .wbm0_we_i (wbm0_we_i),
+        .wbm0_stb_i(wbm0_stb_i),
+        .wbm0_ack_o(wbm0_ack_o),
+        .wbm0_cyc_i(wbm0_cyc_i),
+
+        .wbm1_adr_i(wbm1_adr_i),
+        .wbm1_dat_i(wbm1_dat_i),
+        .wbm1_dat_o(wbm1_dat_o),
+        .wbm1_we_i (wbm1_we_i),
+        .wbm1_stb_i(wbm1_stb_i),
+        .wbm1_ack_o(wbm1_ack_o),
+        .wbm1_cyc_i(wbm1_cyc_i),
+
+        .wbs_adr_o(addr_wb),
+        .wbs_dat_i(data_i_wb),
+        .wbs_dat_o(data_o_wb),
+        .wbs_we_o (wr_en_wb),
+        .wbs_stb_o(stb_wb),
+        .wbs_ack_i(ack_wb),
+        .wbs_cyc_o(cyc_wb)
+    );
+
+
     
     // ----------------------------------
     //         Wishbone Signals
     // ----------------------------------
-    logic [31 : 0]           addr_wb;
+    logic [19 : 0]           addr_wb;
     logic [31 : 0]           data_i_wb;
     logic [31 : 0]           data_o_wb;
     logic                    wr_en_wb;
@@ -53,14 +111,14 @@ module obi_wb_bridge_tb;
         // .ser_rx,
 
         // WB output interface
-        .wb_addr_o      (addr_wb),
-        .wb_rdata_i     (data_i_wb),
-        .wb_wdata_o     (data_o_wb),
-        .wb_wr_en_o     (wr_en_wb),
+        .wb_addr_o      (wbm1_adr_i),
+        .wb_rdata_i     (wbm1_dat_o),
+        .wb_wdata_o     (wbm1_dat_i),
+        .wb_wr_en_o     (wbm1_we_i),
         .wb_byte_en_o   (byte_en_wb),
-        .wb_stb_o       (stb_wb),
-        .wb_ack_i       (ack_wb),
-        .wb_cyc_o       (cyc_wb),
+        .wb_stb_o       (wbm1_stb_i),
+        .wb_ack_i       (wbm1_ack_o),
+        .wb_cyc_o       (wbm1_cyc_i),
 
         // WB input interface to access RAM
         .wb_addr_i      ('0),
@@ -71,9 +129,12 @@ module obi_wb_bridge_tb;
         .wb_cyc_i       ('0)
     );
 
+
     logic timer_sel;
     assign timer_sel = addr_wb[19:8] == 12'b111000000000;
 
+    logic default_slave_stb;
+    assign default_slave_stb = stb_wb & ~timer_sel;
 
     logic default_ack;
     logic [31:0] default_dat;
--- core/custom/obi_wb_bridge/testbench/obi_wb_bridge_test.py
+++ core/custom/obi_wb_bridge/testbench/obi_wb_bridge_test.py
@@ -5,10 +5,21 @@ from cocotb.triggers import Timer, RisingEdge, FallingEdge, ClockCycles
 from cocotbext.wishbone.driver import WishboneMaster, WBOp
 from cocotbext.wishbone.monitor import WishboneSlave
 from cocotb.utils import *
+import random
 
 CLK_PER_SYNC = 300
 SYSCLK = 25e6
 WBCLK  = 100e6
+
+async def run_wbm(wbm, address=0, count=1000, max_ns_between_transactions=10):
+  for i in range(count):
+    if random.randint(0,2):
+      await wbm.send_cycle([WBOp(address, i)])
+    else:
+      await wbm.send_cycle([WBOp(address)])
+
+    await Timer(random.randint(0, max_ns_between_transactions), units='ns')
+
 
 @cocotb.test()
 async def obi_wb_bridge_test(dut):
@@ -17,10 +28,25 @@ async def obi_wb_bridge_test(dut):
 
     dut._log.info("Initialize and reset model")
 
+
     dut.core_rst_n.value = 0
     await Timer(30, units='ns')
     await RisingEdge(dut.core_clk)
     dut.core_rst_n.value = 1
+
+    # Wishbone Master 0
+    wbm0 = WishboneMaster(dut,
+                          "wbm0",
+                          dut.wfg_clk,
+                          width=32,   # size of data bus
+                          timeout=1000, # in clock cycle number
+                          signals_dict={"cyc":  "cyc_i",
+                                        "stb":  "stb_i",
+                                        "we":   "we_i",
+                                        "adr":  "adr_i",
+                                        "datwr":"dat_i",
+                                        "datrd":"dat_o",
+                                        "ack":  "ack_o" })
 
     # wishbone slave 
     wbs = WishboneSlave(dut,
@@ -29,7 +55,7 @@ async def obi_wb_bridge_test(dut):
                         width=32, # size of data bus
                         signals_dict={
                             "cyc": "cyc_wb",
-                            "stb": "stb_wb",
+                            "stb": "default_slave_stb",
                             "we": "wr_en_wb",
                             "adr": "addr_wb",
                             "datwr": "data_o_wb",
@@ -37,9 +63,22 @@ async def obi_wb_bridge_test(dut):
                             "ack": "default_ack"
                         },
                         datgen = iter([0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1, 0x1]))
-    
-    await Timer(185, units='us')
 
+
+    await RisingEdge(dut.wbm1_cyc_i)
+
+    m0 = cocotb.start_soon(run_wbm(wbm=wbm0, address=0, count=50, max_ns_between_transactions=200))
+
+    for _ in range(2):
+        await Timer(50, units='us')
+        dut._log.info("50us")
+
+    '''
     wbs.log.info("received %d transactions" % len(wbs._recvQ))
     for transaction in wbs._recvQ:
         wbs.log.info(f"{[f'@{hex(v.adr)}r{hex(v.datrd)}w{hex(0 if v.datwr is None else v.datwr)}' for v in transaction]}")
+    '''
+
+
+
+    await m0
