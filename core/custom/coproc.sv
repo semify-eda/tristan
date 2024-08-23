a5493f17f8ab1dbcf9f310d1ac571262619e10b3
Christian Tchilikov <christian.tchilikov@semify-eda.com>
2024-08-23 18:22:51 +0200

    control and load datapaths working

--- core/custom/coproc.sv
+++ core/custom/coproc.sv
@@ -1,5 +1,3 @@
-//! TODO: fix imports for icarus
-
 `default_nettype none
 module coproc import coproc_pkg::*;
 #(
@@ -28,21 +26,31 @@ module coproc import coproc_pkg::*;
 );
 
 
-  typedef enum logic [3:0] {
-    IDLE          = 4'b0001,
-    EXECUTE       = 4'b0010,
-    MEM_RESP      = 4'b0100,
-    RETIRE        = 4'b1000
+  // onehot encoding of states
+  typedef enum {
+    IDLE,
+    CFG,
+    MEM_RD1,
+    MEM_RD2,
+    UPDATE,
+    MEM_WR1,
+    MEM_WR2,
+    STALL,
+    RETIRE,
+    INVALID,
+    KILL
   } coproc_state_e;
 
-  // Control Registers
+  /* ====================== Control Registers ====================== */
   logic [31:0] ld_addr;             // address of start of read (load) stream
   logic [31:0] st_addr;             // address of start of write (store) stream
   logic [31:0] shadow_reg;          // shadow data register
   logic [31:0] data_load_reg;       // custom data register
   logic [63:0] rbuf;                // read data buffer
 
-
+  /* ====================== Control Signals ====================== */
+  logic cfg;
+  assign cfg = funct3[2];
   /**
   *   NOTES:
   *     - for now, do not pipeline the coprocessor. This means the input id, rs1, rs2, rd
@@ -51,43 +59,45 @@ module coproc import coproc_pkg::*;
   logic [31:0]    rs1, rs2, rd;
   logic [ 3:0]    id;
   logic           issue_valid_ff;
-  logic           commit_valid, commit_valid_ff;
+  logic           commit_valid,     commit_valid_ff;
   logic           commit_kill;
 
-  // memory signals
+  /* ====================== Memory Signals ====================== */
   logic [31:0]    mem_rdata;
   logic           mem_err, mem_dbg;
 
   coproc_opcode_e opcode;
-  rmst_funct3_e   funct_3;
+  rmst_funct3_e   funct3;
   // FSM
   coproc_state_e state_ff, state_next;
 
   assign opcode = coproc_opcode_e'(xif_issue_if.issue_req.instr[ 6: 0]);
-  assign funct_3 =  rmst_funct3_e'(xif_issue_if.issue_req.instr[14:12]);
+  assign funct3 =  rmst_funct3_e'(xif_issue_if.issue_req.instr[14:12]);
   assign commit_kill = xif_commit_if.commit.commit_kill;
 
   // sticky signals
-  assign commit_valid = commit_valid_ff | xif_commit_if.commit_valid;
+  /* ====================== Sticky Signals ====================== */
+  assign commit_valid     = commit_valid_ff     | xif_commit_if.commit_valid;
   always_ff @(posedge clk_i, negedge rst_ni) begin : commit_monitor
     if(~rst_ni) begin
-      commit_valid_ff <= '0;
-      issue_valid_ff  <= '0;
+      commit_valid_ff     <= '0;
+      issue_valid_ff      <= '0;
     end else begin
       if(xif_commit_if.commit_valid) begin
-        commit_valid_ff <= '1;
+        commit_valid_ff     <= '1;
       end else if(xif_result_if.result_valid) begin
-        commit_valid_ff <= '0;
+        commit_valid_ff     <= '0;
       end
       if(xif_issue_if.issue_valid) begin
-        issue_valid_ff  <= '1;
+        issue_valid_ff      <= '1;
       end else if(xif_result_if.result_valid) begin
-        issue_valid_ff  <= '0;
+        issue_valid_ff      <= '0;
       end
     end
   end : commit_monitor
 
   // Combinational Signals
+  /* ================== Combinational Handshake Signals ====================== */
   assign xif_issue_if.issue_resp.accept     = (opcode == OPCODE_RMLD | opcode == OPCODE_RMST | opcode == OPCODE_TEST);
   assign xif_issue_if.issue_resp.writeback  = (opcode == OPCODE_TEST | opcode == OPCODE_RMST);
 
@@ -108,20 +118,44 @@ module coproc import coproc_pkg::*;
       unique case(state_ff)
         IDLE:
           if(issue_valid_ff) begin
-            state_next = EXECUTE;
-          end
-        EXECUTE:
-          if(commit_valid) begin
-            if((opcode == OPCODE_RMST && (funct_3 == RMXR || funct_3 == RMXS || funct_3 == RMCS || funct_3 == RMCC)) || opcode == OPCODE_RMLD) begin
-              state_next = xif_mem_if.mem_ready ? MEM_RESP : EXECUTE;
+            if(opcode == OPCODE_RMST | opcode == OPCODE_RMLD) begin
+              state_next = cfg ? CFG : MEM_RD1;
             end else begin
-              state_next = RETIRE;
+              state_next = INVALID;
             end
           end
-        MEM_RESP:
-          if(xif_mem_result_if.mem_result_valid) begin
+        CFG:
+          if(commit_valid) begin
             state_next = RETIRE;
+          end else if (commit_kill) begin
+            state_next = KILL;
           end
+        MEM_RD1:
+          if(xif_mem_result_if.mem_result_valid) begin
+            state_next = MEM_RD2;
+          end else if (commit_kill) begin
+            state_next = KILL;
+          end
+        MEM_RD2:
+          if(xif_mem_result_if.mem_result_valid) begin
+            state_next = UPDATE;
+          end else if (commit_kill) begin
+            state_next = KILL;
+          end
+        UPDATE:
+          if(commit_valid) begin
+            state_next = opcode == OPCODE_RMLD ? RETIRE : MEM_WR1;
+          end
+        MEM_WR1:
+          if(xif_mem_result_if.mem_result_valid) begin
+            state_next = MEM_WR2;
+          end
+        MEM_WR2:
+          if(xif_mem_result_if.mem_result_valid) begin
+            state_next = STALL;
+          end
+        STALL:
+          state_next = RETIRE;
         RETIRE:
           if(xif_result_if.result_ready) begin
               state_next = IDLE;
@@ -181,84 +215,110 @@ module coproc import coproc_pkg::*;
       xif_result_if.result.err                  <= '0;
       xif_result_if.result.dbg                  <= '0;
     end else begin
-      xif_result_if.result_valid                <= '0;
-      xif_mem_if.mem_valid                      <= '0;
-      case(state_ff)
-        IDLE: begin
-          rs1 <= xif_issue_if.issue_req.rs[0];
-          rs2 <= xif_issue_if.issue_req.rs[1];
-          rd  <= xif_issue_if.issue_req.instr[11:7];
-          id  <= xif_issue_if.issue_req.id;
-        end
-      endcase
       case(state_next)
-        EXECUTE: begin
+        IDLE: begin
+          xif_issue_if.issue_ready            <= '1;
+          xif_mem_if.mem_valid                <= '0;
+          xif_result_if.result_valid          <= '0;
+        end
+        CFG: begin
+          // set issue ready low
+          xif_issue_if.issue_ready            <= '0;
           xif_issue_if.issue_resp.dualwrite   <= '0;
           xif_issue_if.issue_resp.dualread    <= '0;
           xif_issue_if.issue_resp.loadstore   <= '0;
           xif_issue_if.issue_resp.ecswrite    <= '0;
           xif_issue_if.issue_resp.exc         <= '0;
+
+          if(commit_valid) begin
+            case(funct3)
+              CDSRM: begin
+                data_load_reg <= rs1;
+              end
+              CASRM: begin
+                st_addr       <= rs1;
+              end
+              CALRM: begin
+                ld_addr       <= rs2;
+              end
+              CASLRM: begin
+                st_addr       <= rs1;
+                ld_addr       <= rs2;
+              end
+            endcase
+          end
+        end
+        MEM_RD1: begin
+          // set issue ready low
           xif_issue_if.issue_ready            <= '0;
-          case(opcode)
-            OPCODE_RMLD: begin
-              //! wiggle these signals
-              xif_issue_if.issue_resp.loadstore   <= '1;
-              xif_issue_if.issue_resp.exc         <= '1; //! can cause an exception for
-                                                        //  an incorrect mem address
+          xif_issue_if.issue_resp.dualwrite   <= '0;
+          xif_issue_if.issue_resp.dualread    <= '0;
+          xif_issue_if.issue_resp.loadstore   <= '1;
+          xif_issue_if.issue_resp.ecswrite    <= '0;
+          xif_issue_if.issue_resp.exc         <= '1;
 
-            end
-            OPCODE_RMST: begin
+          // request read from the CPU
+          xif_mem_if.mem_valid                <= '1;
+          xif_mem_if.mem_req.id               <= xif_issue_if.issue_req.id;
+          xif_mem_if.mem_req.addr             <= ld_addr;
+          xif_mem_if.mem_req.mode             <= '1;    // set to machine level for now
+          xif_mem_if.mem_req.we               <= '0;
+          xif_mem_if.mem_req.size             <= 3'h2;  // set to a word (32b)
+          xif_mem_if.mem_req.be               <= '1;    // enable all bytes
+          xif_mem_if.mem_req.attr[1]          <= '1;    // set as modifiable
+          xif_mem_if.mem_req.attr[0]          <= '0;    // set as aligned
+          xif_mem_if.mem_req.last             <= opcode == OPCODE_RMLD;    // declare the memory transaction to be last if its a read instruction
+          xif_mem_if.mem_req.spec             <= '0;    // memory transaction is not speculative
 
-              case(funct_3)
-                RMXR: begin
-                  //! wiggle these signals
-                  xif_mem_if.mem_valid                <= '1;
-                  xif_mem_if.mem_req.id               <= xif_issue_if.issue_req.id;
-                  xif_mem_if.mem_req.addr             <= xif_issue_if.issue_req.rs[0];
-                  xif_mem_if.mem_req.mode             <= '1;    // set to machine level for now
-                  xif_mem_if.mem_req.we               <= '1;
-                  xif_mem_if.mem_req.size             <= 3'h2;  // set to a word (32b)
-                  xif_mem_if.mem_req.be               <= '1;    // enable all bytes
-                  xif_mem_if.mem_req.attr[1]          <= '1;    // set as modifiable
-                  xif_mem_if.mem_req.attr[0]          <= '0;    // set as aligned
-                  xif_mem_if.mem_req.wdata            <= xif_issue_if.issue_req.rs[1];
-                  xif_mem_if.mem_req.last             <= '1;    // declare the memory transaction to be the last for the offloaded instruction
-                  xif_mem_if.mem_req.spec             <= '0;    // memory trasnaction is not speculative
-
-
-                  xif_issue_if.issue_resp.loadstore   <= '1;
-                  xif_issue_if.issue_resp.exc         <= '1; //! can cause an exception for
-                                                            //  an incorrect mem address
-                end
-                RMXS: begin
-                  //TODO
-                end
-                RMCS: begin
-                  //TODO
-                end
-                RMCC: begin
-                  //TODO
-                end
-
-                CDSRM: begin
-                  data_load_reg <= rs1;
-                end
-                CASRM: begin
-                  st_addr <= rs1;
-                end
-                CALRM: begin
-                  ld_addr <= rs1;
-                end
-                CASLRM: begin
-                  ld_addr <= rs1;
-                  st_addr <= rs2;
-                end
-              endcase
-            end
-            OPCODE_TEST: begin
-              //! wiggle these signals
-            end
+          xif_issue_if.issue_resp.loadstore   <= '1;
+          xif_issue_if.issue_resp.exc         <= '1; //! can cause an exception for
+                                                      //  an incorrect mem address
+        end
+        MEM_RD2: begin
+          if(xif_mem_result_if.mem_result_valid) begin
+            rbuf[31:0]                        <= xif_mem_result_if.mem_result.rdata;
+            xif_mem_if.mem_req.addr           <= xif_mem_if.mem_req.addr + 3'b100;
+          end
+        end
+        UPDATE: begin
+          if(xif_mem_result_if.mem_result_valid) begin
+            rbuf[63:32]                       <= xif_mem_result_if.mem_result.rdata;
+          end
+          xif_mem_if.mem_valid                <= '0;
+        end
+        MEM_WR1: begin
+          //! wiggle these signals
+          xif_mem_if.mem_valid                <= '1;
+          xif_mem_if.mem_req.id               <= xif_issue_if.issue_req.id;
+          xif_mem_if.mem_req.addr             <= st_addr;
+          xif_mem_if.mem_req.mode             <= '1;    // set to machine level for now
+          xif_mem_if.mem_req.we               <= '1;
+          xif_mem_if.mem_req.size             <= 3'h2;  // set to a word (32b)
+          xif_mem_if.mem_req.be               <= '1;    // enable all bytes
+          xif_mem_if.mem_req.attr[1]          <= '1;    // set as modifiable
+          xif_mem_if.mem_req.attr[0]          <= '0;    // set as aligned
+          xif_mem_if.mem_req.last             <= '0;    // declare the memory transaction to be the last for the offloaded instruction
+          xif_mem_if.mem_req.spec             <= '0;    // memory trasnaction is not speculative
+          case(funct3)
+            RMCS:
+              xif_mem_if.mem_req.wdata        <= shadow_reg;
+            RMCC:
+              xif_mem_if.mem_req.wdata        <= data_load_reg;
+            RMXR:
+              //TODO: update this value
+              xif_mem_if.mem_req.wdata        <= '0;
+            RMXS:
+              //TODO: update this value
+              xif_mem_if.mem_req.wdata        <= '1;
           endcase
+
+          xif_issue_if.issue_resp.loadstore   <= '1;
+          xif_issue_if.issue_resp.exc         <= '1; //! can cause an exception for
+                                                      //  an incorrect mem address
+        end
+        MEM_WR2: begin
+        end
+        STALL: begin
         end
         RETIRE: begin
           xif_issue_if.issue_ready        <= '1;
@@ -275,9 +335,11 @@ module coproc import coproc_pkg::*;
           xif_result_if.result.err        <= '0;
           xif_result_if.result.dbg        <= '0;
 
+
+          //!TODO: move this to the update data state or similar
           case(opcode)
             OPCODE_RMLD: begin
-              //! wiggle these signals
+              shadow_reg                      <= rbuf[47:16];
             end
             OPCODE_RMST: begin
               //! wiggle these signals
@@ -291,6 +353,14 @@ module coproc import coproc_pkg::*;
               xif_result_if.result.we         <= '1;
             end
           endcase
+        end
+      endcase
+      case(state_ff)
+        IDLE: begin
+          rs1 <= xif_issue_if.issue_req.rs[0];
+          rs2 <= xif_issue_if.issue_req.rs[1];
+          rd  <= xif_issue_if.issue_req.instr[11:7];
+          id  <= xif_issue_if.issue_req.id;
         end
       endcase
     end
--- core/custom/coproc_pkg.sv
+++ core/custom/coproc_pkg.sv
@@ -5,21 +5,21 @@ package coproc_pkg;
 *   https://five-embeddev.com/riscv-user-isa-manual/Priv-v1.12/opcode-map.html#opcodemap
 */
 typedef enum logic [6:0] {
-    OPCODE_RMLD   = 7'h0b,
-    OPCODE_RMST   = 7'h2b,
-    OPCODE_TEST   = 7'h6b
-  } coproc_opcode_e;
+  OPCODE_RMLD   = 7'h0b,
+  OPCODE_RMST   = 7'h2b,
+  OPCODE_TEST   = 7'h6b
+} coproc_opcode_e;
 
 
 typedef enum logic [2:0] {
-    RMXR =  3'b000,
-    RMXS =  3'b001,
-    RMCS =  3'b010,
-    RMCC =  3'b011,
-    CDSRM =  3'b100,
-    CASRM =  3'b101,
-    CALRM =  3'b110,
-    CASLRM =  3'b111
+  RMXR    = 3'b000,
+  RMXS    = 3'b001,
+  RMCS    = 3'b010,
+  RMCC    = 3'b011,
+  CDSRM   = 3'b100,
+  CASRM   = 3'b101,
+  CALRM   = 3'b110,
+  CASLRM  = 3'b111
 } rmst_funct3_e;
 
 endpackage
